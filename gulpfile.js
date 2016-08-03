d0853c3cd266c497baea857af0f922ea8b4b233c
koji kobayashi <kojikobayashi1840@gmail.com>
2016-08-03 22:52:16 +0900

    古いファイルの削除とgulpの準備

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var cache = require('gulp-cached');
+
+gulp.task('sass', function () {
+	sass('sass/**/*.scss', {
+		style: 'compressed',
+		sourcemap: true,
+		cacheLocation: '/.sass-cache'
+	})
+	.on('error', sass.logError)
+	.pipe(cache('sass'))
+	.pipe(autoprefixer(['last 3 version', 'ie >= 9', 'Android 4.0']))
+	.pipe(sourcemaps.write('/maps'))
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('reload', function () {
+	browserSync.reload();
+});
+
+gulp.task('watch', function(){
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	gulp.watch('sass/**/*.scss', ['sass']);
+	gulp.watch(['./*.html'], ['reload']);
+});
+
+gulp.task('default', ['watch']);
--- sass_compile.bat
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-
-::sass --watch -t nested .:.
-::sass --watch -t expented .:.
-sass --watch -t compact .:.
-::sass --watch -t compressed .:.
